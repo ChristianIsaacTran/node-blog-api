28dd26627b339364ee59f18ecf0c97ffcdfaa84b
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-07-20 16:07:01 -0500

    FEAT: added jwt authentication to all comment routes
    Users need to be signed in properly and have a
    valid JWT token before doing anything in the comments.

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,40 +1,55 @@
 const db = require("../models/dbQuery");
+const { passport } = require("../authentication/passportConfig");
 
 // reads all comments in database and sends back a json
-const readAllComments = async (req, res) => {
+const readAllComments = [
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     res.json({
-        testComment: "on readAllComments"
+      testComment: "on readAllComments",
     });
-};
+  },
+];
 
 // reads a single comment based on commentId and sends back a json
-const readComment = async (req, res) => {
+const readComment = [
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     res.json({
-        testComment: "on readComment"
+      testComment: "on readComment",
     });
-};
+  },
+];
 
 // creates a comment to input into database
-const createComment = async (req, res) => {
+const createComment = [
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     res.json({
-        testComment: "on createComment"
+      testComment: "on createComment",
     });
-};
+  },
+];
 
 // updates a comment based on commentId and sends back a json to confirm changes
-const updateComment = async (req, res) => {
+const updateComment = [
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     res.json({
-        testComment: "on updateComment"
+      testComment: "on updateComment",
     });
-};
-
+  },
+];
 
 // delete a comment based on commentId and sends back a json to confirm changes
-const deleteComment = async (req, res) => {
+const deleteComment = [
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
     res.json({
-        testComment: "on deleteComment"
+      testComment: "on deleteComment",
     });
-};
+  },
+];
 
 module.exports = {
   readAllComments,
